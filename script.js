af7018d5a81fc357916385b5e5d8cc6d69f01eb7
Paula Fernandez <pauuagustina@gmail.com>
2023-08-09 21:12:21 -0300

    Corrección de formateo y agregado de algunos comentarios

--- script.js
+++ script.js
@@ -2,231 +2,246 @@ var Secuencia = [];
 var PosicionSecuencia = 0;
 var ModoJugador = false;
 var Puntaje = 0;
-///////////////////////////////////////////////////////////////////
 
+//Obtiene el elemento del botón rojo
 var BotonRojo = document.getElementById("BotonRojo");
 
-BotonRojo.addEventListener('mousedown', function () {
-    if (ModoJugador){
-        PresionarColorRojo();
-    }
+//Escucha el evento de presión del boton del mouse
+BotonRojo.addEventListener("mousedown", function () {
+  //Si el modo jugador esta activo
+  if (ModoJugador) {
+    //Ejecuta la función de presionar boton color rojo
+    PresionarColorRojo();
+  }
 });
-function PresionarColorRojo(){
-    // Cambia el color del botón
-    BotonRojo.classList.add('show');
+
+function PresionarColorRojo() {
+  // Cambia el color del botón agregando la clase css
+  BotonRojo.classList.add("show");
 }
 
-function RetomarColorRojo(){
-    BotonRojo.classList.remove('show');
+function RetomarColorRojo() {
+  // Vuelve al color incial removiendo la clase de css
+  BotonRojo.classList.remove("show");
 }
 
-BotonRojo.addEventListener('mouseup', function () {
-    RetomarColorRojo();
- });
+// Escucha el evento cuando se deja de presionar el boton del mouse
+BotonRojo.addEventListener("mouseup", function () {
+  RetomarColorRojo();
+});
 
- BotonRojo.addEventListener('click', function () {
-    VerificarSecuencia(1);
- });
-
-//////////////////////////////////////////////////////////////////
+// Escucha el evento cuando el mouse hace clcik
+BotonRojo.addEventListener("click", function () {
+  // Ejecuta la función "Verificar secuencia"
+  VerificarSecuencia(1);
+});
 
 var BotonVerde = document.getElementById("BotonVerde");
 
-BotonVerde.addEventListener('mousedown', function () {
-    if (ModoJugador){
-        PresionarColorVerde();
-    }
+BotonVerde.addEventListener("mousedown", function () {
+  if (ModoJugador) {
+    PresionarColorVerde();
+  }
 });
-function PresionarColorVerde(){
-    BotonVerde.classList.add('show');
+
+function PresionarColorVerde() {
+  BotonVerde.classList.add("show");
 }
 
-function RetomarColorVerde(){
-    BotonVerde.classList.remove('show');
+function RetomarColorVerde() {
+  BotonVerde.classList.remove("show");
 }
 
-BotonVerde.addEventListener('mouseup', function () {
-    RetomarColorVerde();
- });
+BotonVerde.addEventListener("mouseup", function () {
+  RetomarColorVerde();
+});
 
- BotonVerde.addEventListener('click', function () {
-    VerificarSecuencia(2);
- });
-
-//////////////////////////////////////////////////////////////
+BotonVerde.addEventListener("click", function () {
+  VerificarSecuencia(2);
+});
 
 var BotonAmarillo = document.getElementById("BotonAmarillo");
 
-BotonAmarillo.addEventListener('mousedown', function () {
-    if (ModoJugador){
-        PresionarColorAmarillo();
-    }
+BotonAmarillo.addEventListener("mousedown", function () {
+  if (ModoJugador) {
+    PresionarColorAmarillo();
+  }
 });
-function PresionarColorAmarillo(){
-    // Cambia el color del botón
-    BotonAmarillo.classList.add('show');
+
+function PresionarColorAmarillo() {
+  // Cambia el color del botón
+  BotonAmarillo.classList.add("show");
 }
 
-function RetomarColorAmarillo(){
-    BotonAmarillo.classList.remove('show');
+function RetomarColorAmarillo() {
+  BotonAmarillo.classList.remove("show");
 }
 
-BotonAmarillo.addEventListener('mouseup', function () {
-    RetomarColorAmarillo();
- });
+BotonAmarillo.addEventListener("mouseup", function () {
+  RetomarColorAmarillo();
+});
 
- BotonAmarillo.addEventListener('click', function () {
-    VerificarSecuencia(3);
- });
-
-///////////////////////////////////////////////////////////////////
+BotonAmarillo.addEventListener("click", function () {
+  VerificarSecuencia(3);
+});
 
 var BotonAzul = document.getElementById("BotonAzul");
 
-BotonAzul.addEventListener('mousedown', function () {
-    if (ModoJugador){
-        PresionarColorAzul();
-    }
+BotonAzul.addEventListener("mousedown", function () {
+  if (ModoJugador) {
+    PresionarColorAzul();
+  }
 });
-function PresionarColorAzul(){
-    // Cambia el color del botón
-    BotonAzul.classList.add('show');
+
+function PresionarColorAzul() {
+  // Cambia el color del botón
+  BotonAzul.classList.add("show");
 }
 
-function RetomarColorAzul(){
-    BotonAzul.classList.remove('show');
+function RetomarColorAzul() {
+  BotonAzul.classList.remove("show");
 }
 
-BotonAzul.addEventListener('mouseup', function () {
-    RetomarColorAzul();
- });
+BotonAzul.addEventListener("mouseup", function () {
+  RetomarColorAzul();
+});
 
- BotonAzul.addEventListener('click', function () {
-    VerificarSecuencia(4);
- });
+BotonAzul.addEventListener("click", function () {
+  VerificarSecuencia(4);
+});
 
-/////////////////////////////////////////////////////////////////
-
-function ElegirColor(){
-    setTimeout(function(){
-        if (PosicionSecuencia == Secuencia.length) { //Llegas al final de la secuencia y selecciono un nuevo color  
-            var NumeroElegido = Math.floor(Math.random() * (4 - 1) + 1);
-            Secuencia.push(NumeroElegido);
-            PresionarSecuencia(NumeroElegido);
-            PosicionSecuencia = PosicionSecuencia + 1;
-        }
-        else if (PosicionSecuencia < Secuencia.length) { //aun esta ejecutando la secuencia, presiono el de la posicion actual
-             PresionarSecuencia(Secuencia[PosicionSecuencia]);
-        }
-        else{ //se recorrio la secuencia, empieza el turno del jugadador
-            PosicionSecuencia = 0;
-            ModoJugador = true;
-            return;
-        }
-        PosicionSecuencia = PosicionSecuencia + 1;
-    }, 1000);
+function ElegirColor() {
+  setTimeout(function () {
+    if (PosicionSecuencia == Secuencia.length) {
+      //Llegas al final de la secuencia y selecciono un nuevo color
+      var NumeroElegido = Math.floor(Math.random() * 3 + 1);
+      Secuencia.push(NumeroElegido);
+      PresionarSecuencia(NumeroElegido);
+      PosicionSecuencia = PosicionSecuencia + 1;
+    } else if (PosicionSecuencia < Secuencia.length) {
+      //aun esta ejecutando la secuencia, presiono el de la posicion actual
+      PresionarSecuencia(Secuencia[PosicionSecuencia]);
+    } else {
+      //se recorrio la secuencia, empieza el turno del jugadador
+      PosicionSecuencia = 0;
+      ModoJugador = true;
+      return;
+    }
+    PosicionSecuencia = PosicionSecuencia + 1;
+  }, 1000);
 }
 
-function VerificarSecuencia(NumeroElegido){
-    if (!ModoJugador){
-        return;
+function VerificarSecuencia(NumeroElegido) {
+  if (!ModoJugador) {
+    return;
+  }
+  if (Secuencia[PosicionSecuencia] == NumeroElegido) {
+    if (PosicionSecuencia == Secuencia.length - 1) {
+      PosicionSecuencia = 0;
+      ModoJugador = false;
+      ElegirColor();
+    } else {
+      PosicionSecuencia = PosicionSecuencia + 1;
     }
-    if (Secuencia[PosicionSecuencia] == NumeroElegido){
-        if (PosicionSecuencia == Secuencia.length -1){
-             PosicionSecuencia = 0;
-             ModoJugador = false;
-             ElegirColor();
-        }
-        else{
-            PosicionSecuencia = PosicionSecuencia + 1;
-        }
-        Puntaje = Puntaje + 1;
-        document.getElementById("Puntuacion").innerHTML = Puntaje;
-    }
-    else {
-        ModoJugador = false;
-        document.querySelector('.ContendorPuntuacion').classList.remove('show');
-        var NombreJugador = document.getElementById("Nombre").value;
-        document.getElementById("ModalMensaje").innerText = `Perdiste ${NombreJugador}`; 
-        document.querySelector("#PuntuacionAnterior").innerText = `Tu puntuación fue: ${Puntaje}`;
-        document.getElementsByClassName("ContenedorModal")[0].classList.add("MostrarModal");
-        // MostrarInscripcion()
-    }
+    Puntaje = Puntaje + 1;
+    document.getElementById("Puntuacion").innerHTML = Puntaje;
+  } else {
+    ModoJugador = false;
+    document.querySelector(".ContendorPuntuacion").classList.remove("show");
+    var NombreJugador = document.getElementById("Nombre").value;
+    document.getElementById(
+      "ModalMensaje"
+    ).innerText = `Perdiste ${NombreJugador}`;
+    document.querySelector(
+      "#PuntuacionAnterior"
+    ).innerText = `Tu puntuación fue: ${Puntaje}`;
+    document
+      .getElementsByClassName("ContenedorModal")[0]
+      .classList.add("MostrarModal");
+    // MostrarInscripcion()
+  }
 }
 
-function PresionarSecuencia(NumeroElegido){
-    switch (NumeroElegido) {
-        case 1: 
-            PresionarColorRojo()
-            setTimeout(function(){
-                RetomarColorRojo()
-                ElegirColor()
-            }, 1000);
-            break;
-        case 2: 
-            PresionarColorVerde()
-            setTimeout(function(){
-                RetomarColorVerde()
-                ElegirColor()
-            }, 1000);
-            break;
-        case 3: 
-            PresionarColorAmarillo()
-            setTimeout(function(){
-                RetomarColorAmarillo()
-                ElegirColor()
-            }, 1000);
-            break;
-        case 4: 
-            PresionarColorAzul()
-            setTimeout(function(){
-                RetomarColorAzul()
-                ElegirColor()
-            }, 1000);
-            break;
-    
-        default:
-            break;
-    }
+function PresionarSecuencia(NumeroElegido) {
+  switch (NumeroElegido) {
+    case 1:
+      PresionarColorRojo();
+      setTimeout(function () {
+        RetomarColorRojo();
+        ElegirColor();
+      }, 1000);
+      break;
+    case 2:
+      PresionarColorVerde();
+      setTimeout(function () {
+        RetomarColorVerde();
+        ElegirColor();
+      }, 1000);
+      break;
+    case 3:
+      PresionarColorAmarillo();
+      setTimeout(function () {
+        RetomarColorAmarillo();
+        ElegirColor();
+      }, 1000);
+      break;
+    case 4:
+      PresionarColorAzul();
+      setTimeout(function () {
+        RetomarColorAzul();
+        ElegirColor();
+      }, 1000);
+      break;
+
+    default:
+      break;
+  }
 }
 
 var BotonInicio = document.getElementById("BotonInicio");
 
-function ComenzarJuego(){
-    PosicionSecuencia = 0;
-    Secuencia = []; 
-    ModoJugador = false;
-    ElegirColor();
+function ComenzarJuego() {
+  PosicionSecuencia = 0;
+  Secuencia = [];
+  ModoJugador = false;
+  ElegirColor();
 }
 
-
 function MostrarInscripcion() {
-    document.querySelector(".MenuContenedor").classList.add('show');
- }
+  document.querySelector(".MenuContenedor").classList.add("show");
+}
 
-BotonInicio.addEventListener('click', function () {
-    BotonInicio.style.display = "none"
-    document.querySelector('.ContendorPuntuacion').classList.add('show')
-    MostrarInscripcion()
+BotonInicio.addEventListener("click", function () {
+  BotonInicio.style.display = "none";
+  document.querySelector(".ContendorPuntuacion").classList.add("show");
+  MostrarInscripcion();
 });
 
-document.getElementById("BotonNombre").addEventListener("click", function(event) {
+document
+  .getElementById("BotonNombre")
+  .addEventListener("click", function (event) {
     var NombreJugador = document.getElementById("Nombre").value;
     // Comprobar si el valor ingresado cumple con el patrón
     if (NombreJugador.length < 3) {
-        MostrarModal("Ingrese mínimo 3 letras");
-        return;
+      MostrarModal("Ingrese mínimo 3 letras");
+      return;
     }
-    document.querySelector(".MenuContenedor").classList.remove('show');
+    document.querySelector(".MenuContenedor").classList.remove("show");
     ComenzarJuego();
-});
+  });
 
-document.getElementById("CerrarModal").addEventListener("click", function(event) {
-    document.getElementsByClassName("ContenedorModal")[0].classList.remove("MostrarModal");
+document
+  .getElementById("CerrarModal")
+  .addEventListener("click", function (event) {
+    document
+      .getElementsByClassName("ContenedorModal")[0]
+      .classList.remove("MostrarModal");
     MostrarInscripcion();
-});
+  });
 
 function MostrarModal(Mensaje) {
-    document.getElementById("ModalMensaje").innerText = Mensaje; 
-    document.getElementsByClassName("ContenedorModal")[0].classList.add("MostrarModal");
+  document.getElementById("ModalMensaje").innerText = Mensaje;
+  document
+    .getElementsByClassName("ContenedorModal")[0]
+    .classList.add("MostrarModal");
 }
--- scriptFormulario.js
+++ scriptFormulario.js
@@ -1,63 +1,85 @@
 var BotonEnviar = document.getElementById("EnviarContacto");
-BotonEnviar.addEventListener('click', function () {
-    var CampoNombre = document.getElementById("NombreContacto");
-    var CampoCorreo = document.getElementById("CorreoContacto");
-    var CampoMensaje = document.getElementById("MensajeContacto");
+BotonEnviar.addEventListener("click", function () {
+  var CampoNombre = document.getElementById("NombreContacto");
+  var CampoCorreo = document.getElementById("CorreoContacto");
+  var CampoMensaje = document.getElementById("MensajeContacto");
 
-    if (CampoNombre.value.length === 0) {
-        MostrarModal("Debe ingresar un nombre.");
-        return;
-    }
+  if (CampoNombre.value.length === 0) {
+    MostrarModal("Debe ingresar un nombre.");
+    return;
+  }
 
-    if (!EsAlfanumerico(CampoNombre.value)) {
-        MostrarModal("El nombre debe completarse con valores alfanumericos.");
-        return;
-    }
+  if (!EsAlfanumerico(CampoNombre.value)) {
+    MostrarModal("El nombre debe completarse con valores alfanumericos.");
+    return;
+  }
 
-    if (CampoCorreo.value.length === 0) {
-        MostrarModal("Debe ingresar un correo.");
-        return;
-    }
+  if (CampoCorreo.value.length === 0) {
+    MostrarModal("Debe ingresar un correo.");
+    return;
+  }
 
-    if (!CorreoValido(CampoCorreo.value)) {
-        MostrarModal("El formato del correo no es correcto.");
-        return;
-    }
+  if (!CorreoValido(CampoCorreo.value)) {
+    MostrarModal("El formato del correo no es correcto.");
+    return;
+  }
 
-    if (CampoMensaje.value.length < 5) {
-        MostrarModal("El mensaje debe tener al menos 5 caracteres.");
-        return;
-    }
-    
-    location.href = "mailto:contacto@simon.com?subject=Contacto de " + CampoNombre.value  + "&body=" + "Nombre: " + CampoNombre.value + ". Correo: " + CampoCorreo.value + ". Mensaje: " + CampoMensaje.value;
+  if (CampoMensaje.value.length < 5) {
+    MostrarModal("El mensaje debe tener al menos 5 caracteres.");
+    return;
+  }
+
+  location.href =
+    "mailto:contacto@simon.com?subject=Contacto de " +
+    CampoNombre.value +
+    "&body=" +
+    "Nombre: " +
+    CampoNombre.value +
+    ". Correo: " +
+    CampoCorreo.value +
+    ". Mensaje: " +
+    CampoMensaje.value;
 });
 
 function EsAlfanumerico(Cadena) {
-    var Tamaño;
-  
-    for (var i = 0, Tamaño = Cadena.length; i < Tamaño; i++) {
-      var CodigoLetra = Cadena.charCodeAt(i);
-      if (!(CodigoLetra > 47 && CodigoLetra < 58) && // Numero (0-9)
-          !(CodigoLetra > 64 && CodigoLetra < 91) && // Letras Mayusculas (A-Z)
-          !(CodigoLetra > 96 && CodigoLetra < 123)) { // Letras Minusculas (a-z)
-        return false;
-      }
+  var Tamaño;
+
+  for (var i = 0, Tamaño = Cadena.length; i < Tamaño; i++) {
+    var CodigoLetra = Cadena.charCodeAt(i);
+    if (
+      !(CodigoLetra > 47 && CodigoLetra < 58) && // Numero (0-9)
+      !(CodigoLetra > 64 && CodigoLetra < 91) && // Letras Mayusculas (A-Z)
+      !(CodigoLetra > 96 && CodigoLetra < 123)
+    ) {
+      // Letras Minusculas (a-z)
+      return false;
     }
+  }
 
-    return true;
-};
-
-function CorreoValido(Correo) {
-    var PosicionArroba = Correo.lastIndexOf('@');
-    var PosicionUltimoPunto = Correo.lastIndexOf('.');
-    return (PosicionArroba < PosicionUltimoPunto && PosicionArroba > 0 && Correo.indexOf('@@') == -1 && PosicionUltimoPunto > 2 && (Correo.length - PosicionUltimoPunto) > 2);
+  return true;
 }
 
-document.getElementById("CerrarModal").addEventListener("click", function() {
-    document.getElementsByClassName("ContenedorModal")[0].classList.remove("MostrarModal");
+function CorreoValido(Correo) {
+  var PosicionArroba = Correo.lastIndexOf("@");
+  var PosicionUltimoPunto = Correo.lastIndexOf(".");
+  return (
+    PosicionArroba < PosicionUltimoPunto &&
+    PosicionArroba > 0 &&
+    Correo.indexOf("@@") == -1 &&
+    PosicionUltimoPunto > 2 &&
+    Correo.length - PosicionUltimoPunto > 2
+  );
+}
+
+document.getElementById("CerrarModal").addEventListener("click", function () {
+  document
+    .getElementsByClassName("ContenedorModal")[0]
+    .classList.remove("MostrarModal");
 });
 
 function MostrarModal(Mensaje) {
-    document.getElementById("ModalMensaje").innerText = Mensaje; 
-    document.getElementsByClassName("ContenedorModal")[0].classList.add("MostrarModal");
+  document.getElementById("ModalMensaje").innerText = Mensaje;
+  document
+    .getElementsByClassName("ContenedorModal")[0]
+    .classList.add("MostrarModal");
 }
